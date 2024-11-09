53290bd8293cc8ca958d44bacd37d7baf5ccfa82
claytongentry <cg3ntry@gmail.com>
2024-11-09 08:28:55 -0500

    nicen

--- spec/support/shared_contexts/with_template_files.rb
+++ spec/support/shared_contexts/with_template_files.rb
@@ -2,18 +2,21 @@
 
 RSpec.shared_context "with template files" do
   before do
-    FileUtils.mkdir_p(template_dir)
-    File.write(
-      template_dir.join("workspace_template.yml"),
-      "vectors:\nmetrics:"
-    )
-    File.write(
-      template_dir.join("vector_template.yml"),
-      "attributes:"
-    )
+    template_dir.mkpath
+
+    File.write(workspace_template_path, "vectors:\nmetrics:")
+    File.write(vector_template_path, "attributes:")
   end
 
   def template_dir
     Pathname.new(File.dirname(__FILE__)).join("../../../lib/manifolds/templates")
+  end
+
+  def vector_template_path
+    template_dir.join("vector_template.yml")
+  end
+
+  def workspace_template_path
+    template_dir.join("workspace_template.yml")
   end
 end
